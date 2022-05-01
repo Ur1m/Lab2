9580d29bb4f3b38a360b146a6c4fe6571f36525c
Ur1m <83430756+Ur1m@users.noreply.github.com>
2022-05-01 06:10:18 +0200

    ProductController finished

--- Play.Products/src/Products.Service/Controllers/ProductsController.cs
+++ Play.Products/src/Products.Service/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Products.Service.Dtos;
 
@@ -24,5 +25,63 @@ namespace Products.Service.Controllers
             return items;
         }
 
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetById(Guid id)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            var updateItem = item with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price
+            };
+            var index = items.FindIndex(item => item.Id == id);
+            items[index] = updateItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var item = items.FindIndex(item => item.Id == id);
+
+            if (item < 0)
+            {
+                return NotFound();
+            }
+            items.RemoveAt(item);
+
+            return NoContent();
+        }
     }
 }
