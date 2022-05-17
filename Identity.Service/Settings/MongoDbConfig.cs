0ff2b788035261f3ae531f3f34c40b9001cec786
rinorhysenii <83431543+rinorhysenii@users.noreply.github.com>
2022-05-17 16:09:05 +0200

    rename folder identity

---
+++ Identity.Service/Settings/MongoDbConfig.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace Identity.Service.Settings
+{
+    public class MongoDbConfig
+    {
+        public string Name { get; init; }
+        public string Host { get; init; }
+        public int Port { get; init; }
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+    }
+}
