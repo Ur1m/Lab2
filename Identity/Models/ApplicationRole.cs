60711a4a24c23cbd2b9565e0a2a0b8d25e6ca688
rinorhysenii <83431543+rinorhysenii@users.noreply.github.com>
2022-05-17 15:42:21 +0200

    Identity Service

---
+++ Identity/Models/ApplicationRole.cs
@@ -0,0 +1,12 @@
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+using System;
+ 
+namespace Identity.Models
+{
+    [CollectionName("Roles")]
+    public class ApplicationRole : MongoIdentityRole<Guid>
+    {
+ 
+    }
+}
---
+++ Identity/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+using System;
+ 
+namespace Identity.Models
+{
+    [CollectionName("Users")]
+    public class ApplicationUser : MongoIdentityUser<Guid>
+    {
+    }
+}
