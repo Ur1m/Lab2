b41fd01870e45c8e75d747a503b6b363cbfb736e
rinorhyseni <rinorhyseni18@gmail.com>
2022-06-12 16:39:46 +0200

    IdentityAuthenticationService changes

---
+++ IdentityAuthenticationService/IdentityAuthenticationService/Controllers/SecuredController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAuthenticationService.Controllers
+{
+
+    [Authorize(Roles = "Admin")]
+    public class SecuredController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
