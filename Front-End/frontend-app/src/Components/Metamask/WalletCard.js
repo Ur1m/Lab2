94441a633f48a91c667f0f1bb684bec9babccfa6
Ur1m <urimbekteshi7@gmail.com>
2022-07-18 22:02:15 +0200

    Add Metamask Feature

---
+++ Front-End/frontend-app/src/Components/Metamask/WalletCard.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+import {ethers} from 'ethers'
+import './WalletCard.css'
+import "./../../Css/bootstrap.css";
+
+<script src="https://cdn.ethers.io/lib/ethers-5.0.umd.min.js" type="text/javascript">
+</script>
+
+const WalletCard = () => {
+
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+			console.log('MetaMask Here!');
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+				getAccountBalance(result[0]);
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			
+			});
+
+		} else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+	// update account, will cause component re-render
+	const accountChangedHandler = (newAccount) => {
+		setDefaultAccount(newAccount);
+		getAccountBalance(newAccount.toString());
+	}
+
+	const getAccountBalance = (account) => {
+		window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
+		.then(balance => {
+			setUserBalance(ethers.utils.formatEther(balance));
+		})
+		.catch(error => {
+			setErrorMessage(error.message);
+		});
+	};
+
+	const chainChangedHandler = () => {
+		// reload the page to avoid any errors with chain change mid use of application
+		window.location.reload();
+	}
+
+
+	// listen for account changes
+	window.ethereum.on('accountsChanged', accountChangedHandler);
+
+	window.ethereum.on('chainChanged', chainChangedHandler);
+	
+	return (
+		<div className='walletCard'>
+		<h4> Connect to Metamask wallet </h4>
+			<button className="btn btn-btn btn-outline-primary" onClick={connectWalletHandler}>{connButtonText}</button>
+			<div className='accountDisplay'>
+				<p>Address: {defaultAccount}</p>
+			</div>
+			<div className='balanceDisplay'>
+				<p>Balance: {userBalance}</p>
+			</div>
+			{errorMessage}
+		</div>
+	);
+}
+
+export default WalletCard;
