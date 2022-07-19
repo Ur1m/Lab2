fbc4d2f0f52fcef1c99628c69e6ea06f27476d41
berat422 <75537316+berat422@users.noreply.github.com>
2022-07-20 00:01:24 +0200

    //

--- Front-End/frontend-app/src/Components/Metamask/WalletCard.js
+++ Front-End/frontend-app/src/Components/Metamask/WalletCard.js
@@ -16,94 +16,10 @@ import Typography from "@mui/material/Typography";
 ></script>;
 
 const WalletCard = () => {
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [defaultAccount, setDefaultAccount] = useState(null);
-  const [userBalance, setUserBalance] = useState(null);
-  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
-
-  const connectWalletHandler = () => {
-    if (window.ethereum && window.ethereum.isMetaMask) {
-      console.log("MetaMask Here!");
-
-      window.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then((result) => {
-          accountChangedHandler(result[0]);
-          setConnButtonText("Wallet Connected");
-          getAccountBalance(result[0]);
-        })
-        .catch((error) => {
-          setErrorMessage(error.message);
-        });
-    } else {
-      console.log("Need to install MetaMask");
-      setErrorMessage("Please install MetaMask browser extension to interact");
-    }
-  };
-
-  // update account, will cause component re-render
-  const accountChangedHandler = (newAccount) => {
-    setDefaultAccount(newAccount);
-    getAccountBalance(newAccount.toString());
-  };
-
-  const getAccountBalance = (account) => {
-    window.ethereum
-      .request({ method: "eth_getBalance", params: [account, "latest"] })
-      .then((balance) => {
-        setUserBalance(ethers.utils.formatEther(balance));
-      })
-      .catch((error) => {
-        setErrorMessage(error.message);
-      });
-  };
-
-  const chainChangedHandler = () => {
-    // reload the page to avoid any errors with chain change mid use of application
-    window.location.reload();
-  };
-
-  // listen for account changes
-  window.ethereum.on("accountsChanged", accountChangedHandler);
-
-  window.ethereum.on("chainChanged", chainChangedHandler);
-
+  
   return (
     <div className="walletCard">
-      <Card sx={{ display: "flex" }}>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <CardContent sx={{ flex: "1 0 auto" }}>
-            <Typography component="div" variant="h5">
-              <h4> Connect to Metamask wallet </h4>
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              color="text.secondary"
-              component="div"
-            >
-              <button
-                className="btn btn-btn btn-outline-primary"
-                onClick={connectWalletHandler}
-              >
-                {connButtonText}
-              </button>
-              <div className="accountDisplay">
-                <p>Address: {defaultAccount}</p>
-              </div>
-              <div className="balanceDisplay">
-                <p>Balance: {userBalance}</p>
-              </div>
-            </Typography>
-          </CardContent>
-        </Box>
-        <CardMedia
-          className="classes.media"
-          component="img"
-          height="400px"
-          image="/img/metamask1.png"
-          alt="green iguana"
-        />
-      </Card>
+      
     </div>
   );
 };
