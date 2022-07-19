90c534ea73f0930720d9c870ae38fc46112a4fd1
rinorhysenii <rh47818@ubt-uni.net>
2022-07-20 00:09:23 +0200

    route to courses after login success & some return fixes fully

--- Front-End/frontend-app/src/Account/Login/ForgotPassword.js
+++ Front-End/frontend-app/src/Account/Login/ForgotPassword.js
@@ -5,61 +5,62 @@ import axios from "axios";
 import styless from "../Login/styless.css"
 
 export const ForgotPassword = () => {
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    const [data, setData] = useState();
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState();
+  const [isError, setIsError] = useState();
 
-    useEffect(() => {}, []);
-  
-    async function login() {
-      let item = { email };
-      let result = await axios.post(
-        "http://localhost:5000/api/Account/forgot-password",
-        item
-      );
+  useEffect(() => { }, []);
 
-      if(result.status == 200){
-        setData("A reset link has been sent!")
-        setEmail('');
-      }else{
-        setData(result.statusText);
-      }
-    }
-  
-    return (
+  async function login() {
+    let item = { email };
+    let result = await axios.post(
+      "http://localhost:5000/api/Account/forgot-password",
+      item
+    ).then(res => {
+      setIsError(false);
+      setData("A reset link has been sent!")
+      setEmail('');
+    })
+      .catch(err => {
+        setIsError(true);
+        setData(err.response.statusText);
+      });
+  }
+
+  return (
     <div className="wraper">
-      
-    <div className="containerr">
-      <div className="textbox">
-        <h3>Reset Your Password</h3>
-        <p className="paragrafff">Lost your password? Please enter your email address. <br/>You will receive a link to create a new password via email.</p>
-      </div>
-      <div className="col-sm-4 ">
-        <input
-          type="text"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
-        />
-        <br/>
-        <button onClick={login} className="btn btn-primary">
-          Reset
-        </button>
-        <br/>
-        <br/>
-        {data && (
-        <div className='form-group'>
-          <div
-            className={data ? 'alert alert-success' : 'alert alert-danger'}
-            role='alert'
-          >
-            {data}
-          </div>
+
+      <div className="containerr">
+        <div className="textbox">
+          <h3>Reset Your Password</h3>
+          <p className="paragrafff">Lost your password? Please enter your email address. <br />You will receive a link to create a new password via email.</p>
         </div>
-      )}
+        <div className="col-sm-4 ">
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+          <br />
+          <button onClick={login} className="btn btn-primary">
+            Reset
+          </button>
+          <br />
+          <br />
+          {data && (
+            <div className='form-group'>
+              <div
+                className={isError ? 'alert alert-danger' : 'alert alert-success'}
+                role='alert'
+              >
+                {data}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <div>{setMessage}</div>
-    </div>
     </div>
   );
 };
--- Front-End/frontend-app/src/Account/Login/ResetPassword.js
+++ Front-End/frontend-app/src/Account/Login/ResetPassword.js
@@ -10,65 +10,68 @@ export const ResetPassword = () => {
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
   const [data, setData] = useState();
-  useEffect(() => {}, []);
+  const [isError, setIsError] = useState();
+  useEffect(() => { }, []);
 
   async function reset() {
     let item = { email, password, token };
     let result = await axios.post(
       "http://localhost:5000/api/Account/reset-password",
       item
-    );
-
-    if(result.status == 200){
+    ).then(res => {
+      setIsError(false);
       setData("Password changed succesfully!")
       setEmail('');
       setPassword('');
-    }else{
-      setData(result.data);
-    }
+    })
+      .catch(err => {
+        setIsError(true);
+        setData(err.response.statusText);
+      });
+
   }
 
   return (
     <div className="wraper">
-      
-    <div className="containerr">
-      <div className="textbox">
-        <h3>Reset Your Password</h3>
-        <p className="paragrafff">Please enter your new password</p>
+
+      <div className="containerr">
+        <div className="textbox">
+          <h3>Reset Your Password</h3>
+          <p className="paragrafff">Please enter your new password</p>
+        </div>
+        <div className="col-sm-4 ">
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="New Password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+          />
+          <br />
+          <button onClick={reset} className="btn btn-primary">
+            Reset
+          </button>
+          <br />
+          <br />
+          {data && (
+            <div className='form-group'>
+              <div
+                className={isError ? 'alert alert-danger' : 'alert alert-success'}
+                role='alert'
+              >
+                {data}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="col-sm-4 ">
-        <input
-          type="text"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="New Password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="form-control"
-        />
-        <br />
-        <button onClick={reset} className="btn btn-primary">
-          Reset
-        </button>
-        <br/>
-        <br/>
-        {data && (
-  <div className='form-group'>
-    <div
-      className={data ? 'alert alert-success' : 'alert alert-danger'}
-      role='alert'
-    >
-      {data}
-    </div>
-  </div>
-)}
-      </div>
-    </div>
-    
+
     </div>
   );
 };
