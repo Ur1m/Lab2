317a0cba762ff088bb476a390ca56dee41543599
Ur1m <83430756+Ur1m@users.noreply.github.com>
2023-02-23 01:38:47 +0100

    gg push

--- UserCourseInteraction/UserCourseInteraction/UserCourseInteraction/Models/ProductDto.cs
+++ UserCourseInteraction/UserCourseInteraction/UserCourseInteraction/Models/ProductDto.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Event.ProductsContract
+namespace UserCourseInteraction.Models
 {
-    public class ProductEventDTO
+    public class ProductDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
