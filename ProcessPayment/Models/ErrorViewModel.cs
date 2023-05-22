00e2dbc144a247e85410ec309626f01053b5220d
vlerashehu <117745118+vlerasp@users.noreply.github.com>
2023-05-22 13:20:21 +0200

    Process Payment module

---
+++ ProcessPayment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProcessPayment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProcessPayment/Services/MakePayment.cs
@@ -0,0 +1,56 @@
+﻿using Stripe;
+using System.Threading.Tasks;
+
+namespace ProcessPayment.Services
+{
+    public class MakePayment : IMakePayment
+    {
+        public async Task<dynamic> PayAsync(string cardNumber, string month, string year, string cvc, int value)
+        {
+            try
+            {
+                StripeConfiguration.ApiKey = "sk_test_51N93hdIyYXMuwulxAyhjXshW5zBL0WSl34ynk6XniOXAz0BcyvlWVkp1NZagRBKSChCTYh3reFWIPnlDB2zwFev400y8Ig5OFV";
+                var optionstoken = new TokenCreateOptions
+                {
+                    Card = new Stripe.TokenCardOptions
+                    {
+
+                        Number = cardNumber,
+                        ExpMonth = month,
+                        ExpYear = year,
+                        Cvc = cvc
+                    }
+                };
+
+                var serviceToken = new TokenService();
+                Token stripeToken = await serviceToken.CreateAsync(optionstoken);
+
+                var options = new ChargeCreateOptions
+                {
+                    Amount = value,
+                    Currency = "usd",
+                    Description = "Test",
+                    Source = stripeToken.Id
+                };
+
+                var service = new ChargeService();
+                Charge charge = await service.CreateAsync(options);
+
+                if (charge.Paid)
+                {
+                    return "succesfully";
+                }
+                else
+                {
+                    return "failed";
+                }
+
+            }
+            catch (System.Exception e)
+            {
+
+                throw;
+            }
+        }
+    }
+}
